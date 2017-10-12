fb35eb399d6bf09aa99f75600a5615794512100e
James K. Ryan III <james.k.ryan.iii@gmail.com>
2017-10-12 19:17:35 -0400

    feat(express): Connect Express Server to Client app for Dev and Prod
    Using a proxy strategy for development similar to the one used in .net
    core. For production, the app serves static assets from the compiled
    dist folder in the client app.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const isNotApiRequest = (pathName) => {
+  return !pathName.match('^/api');
+};
+
+if (isDevelopment) {
+  const proxy = require('http-proxy-middleware')
+  app.use('/', proxy(isNotApiRequest, { target: 'http://localhost:8080', changeOrigin: true }));
+} else {
+  const path = require('path');
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`----> App is running at http://0.0.0.0:${port}...`)
